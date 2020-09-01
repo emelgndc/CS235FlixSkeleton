2f26d3104f86f4cb30d02ac974fdefba7da3fc95
Andy <chena14139@gmail.com>
2020-09-02 10:47:10 +1200

    Commented out non-relevant print statements in cs235init.py

--- cs235init.py
+++ cs235init.py
@@ -5,10 +5,10 @@ def main():
     filename = 'datafiles/Data1000Movies.csv'
     movie_file_reader = MovieFileCSVReader(filename)
     movie_file_reader.read_csv_file()
-    print(f'number of unique movies: {len(movie_file_reader.dataset_of_movies)}')
-    print(f'number of unique actors: {len(movie_file_reader.dataset_of_actors)}')
-    print(f'number of unique directors: {len(movie_file_reader.dataset_of_directors)}')
-    print(f'number of unique genres: {len(movie_file_reader.dataset_of_genres)}')
+    # print(f'number of unique movies: {len(movie_file_reader.dataset_of_movies)}')
+    # print(f'number of unique actors: {len(movie_file_reader.dataset_of_actors)}')
+    # print(f'number of unique directors: {len(movie_file_reader.dataset_of_directors)}')
+    # print(f'number of unique genres: {len(movie_file_reader.dataset_of_genres)}')
 
 
 if __name__ == "__main__":
